921ebc3affff3035172138b00bbbed8c1031f406
Gauravlawat <gauravsoni32443@gmail.com>
2024-12-10 17:23:26 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,8 @@
+let score="33"
+console.log(typeof score);
+let valueofscore=Number(score)
+console.log(typeof valueofscore)
+let score2=null
+let vofsc2=Number(score2)
+console.log(vofsc2);
+ // Boolean String Number
