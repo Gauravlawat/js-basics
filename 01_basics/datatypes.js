64d962f44d44e10a9b87be456a961b0b8f426e4c
Gauravlawat <gauravsoni32443@gmail.com>
2024-12-10 17:02:47 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all code as new JS version
+
+//alert(3+3) throws error we use node js not browser
+let name="gaurav"
+let age=21
+let isLogin=false
+//number 2 pow 53
+//bigint
+//string ""
+//boolean true/false
+//null standalone value
+//undefined no assign value
+//symbol unique
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 1727
+let accountEmail="gauravsoni@gmail.com"
+var accountPass="12345"
+accountCity="jaipur"
+let accountState;
+// accountId=2
+accountEmail="gaurav@gmail.com"
+accountCity="Delhi"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState]);
