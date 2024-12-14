8f6850db64fd7ff28be5fa3a64b5826f05cb5328
Gauravlawat <gauravsoni32443@gmail.com>
2024-12-14 09:22:37 +0000

    maths

---
+++ 01_basics/math.js
@@ -0,0 +1,28 @@
+const score =400
+const balance = new Number(100)
+console.log(balance)
+// console.log(score);
+
+// console.log(balance.toString().length);
+
+// console.log(typeof(balance.toString())); //converts to string
+// console.log(balance.toFixed(2)); //number after decimal
+const otherno = 1234.8966
+// console.log(otherno.toPrecision(3))
+const hundreds= 1230123
+// console.log(hundreds.toLocaleString()); //converts to US standards
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// MATHS //
+
+// console.log(Math) //object
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.67));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+const min =10
+const max=20
+console.log((Math.floor(Math.random() * (max-min+1))) + min)
+
