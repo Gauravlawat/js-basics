6bc4657a3f72901826106d7e9640d125f0839207
Gauravlawat <gauravsoni32443@gmail.com>
2024-12-14 11:12:25 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,28 @@
+const arr =[1,3,2,4,true]
+// console.log(arr[4])
+
+const myarr=new Array(1,2,3,4,5)
+// console.log(myarr[2]);
+
+//methods
+myarr.push(6)
+myarr.pop()
+// console.log(myarr);
+myarr.unshift(-1)
+// console.log(myarr); // insert at start index
+myarr.shift(); //pop at start
+
+// console.log(myarr.includes(3))
+// console.log(myarr.indexOf(3));
+const newarr=myarr.join()
+// console.log(newarr)
+// console.log(typeof(newarr))
+
+console.log("a",myarr)
+
+const myn1=myarr.slice(1,3) //myarr remains same
+console.log(myn1)
+console.log(myarr)
+const myn2=myarr.splice(1,3) //myarr changed
+console.log(myn2)
+console.log(myarr)
