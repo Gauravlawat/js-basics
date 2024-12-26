86939234b4a06f1c3cdc2f2716d960f5b61ee5ee
Gauravlawat <gauravsoni32443@gmail.com>
2024-12-26 08:05:03 +0000

    do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,17 @@
+// let i=0;
+// while(i<=5){
+//     console.log(`value of i is ${i}`);
+//     i=i+2
+// }
+// let myArr=["false","true","null"]
+// let l=0;
+// while(l<myArr.length){
+//     console.log(`value is ${myArr[l]}`);
+//     l++;
+    
+// }
+let score=11
+do{
+    console.log(`score is ${score}`)
+    score++
+}while(score<=10)  //execute at least one time
